46ef09fed6b27cedf219c0b27ccb26d6df26bc98
William Trindade <williamtrindade777@gmail.com>
2024-06-16 18:12:24 -0300

    Update Aula

--- fat32/src/main/java/br/ufsm/politecnico/csi/so/Disco.java
+++ fat32/src/main/java/br/ufsm/politecnico/csi/so/Disco.java
@@ -6,23 +6,24 @@ import java.io.IOException;
 import java.io.RandomAccessFile;
 
 public class Disco {
+
     private static final String NOME_ARQUIVO = "fat32.data";
-    public static final int NUM_BLOCOS = 16 * 1024;
-    private static final int TAM_BLOCO = 64*1024;
+    public static final int NUM_BLOCOS = 16*1024;
+    public static final int TAM_BLOCO = 64*1024;
     private RandomAccessFile disco;
 
-    public Disco() throws IOException {
+    public boolean init() throws IOException {
         File f = new File(NOME_ARQUIVO);
-
-        // LEIO
+        //le arquivo
+        boolean existe = f.exists();
         this.disco = new RandomAccessFile(f, "rws");
         this.disco.setLength(NUM_BLOCOS * TAM_BLOCO);
-
+        return existe;
     }
 
     public byte[] leBloco(int number) throws IOException {
         if (number < 0 || number >= NUM_BLOCOS) {
-            throw new IllegalArgumentException("Numero de bloco invalido. deve estar entre 0 e " + NUM_BLOCOS);
+            throw new IllegalArgumentException("Numero de bloco invalido. Deve estar entre 0 e " + NUM_BLOCOS);
         }
         byte[] data = new byte[TAM_BLOCO];
         this.disco.seek(number * TAM_BLOCO);
@@ -32,12 +33,13 @@ public class Disco {
 
     public void escreveBloco(int number, byte[] data) throws IOException {
         if (number < 0 || number >= NUM_BLOCOS) {
-            throw new IllegalArgumentException("Numero de bloco invalido. deve estar entre 0 e " + NUM_BLOCOS);
+            throw new IllegalArgumentException("Numero de bloco invalido. Deve estar entre 0 e " + NUM_BLOCOS);
         }
         if (data.length > TAM_BLOCO) {
-            throw new IllegalArgumentException("Tamanho do bloco não pode exceder ");
+            throw new IllegalArgumentException("tamanho do bloco não pode exceder 64KB.");
         }
         this.disco.seek(number * TAM_BLOCO);
         this.disco.write(data);
     }
+
 }
--- fat32/src/main/java/br/ufsm/politecnico/csi/so/Fat32FS.java
+++ fat32/src/main/java/br/ufsm/politecnico/csi/so/Fat32FS.java
@@ -1,30 +1,110 @@
 package br.ufsm.politecnico.csi.so;
 
+import java.io.*;
 import java.nio.ByteBuffer;
 import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
 
-public class Fat32FS {
+public class Fat32FS implements FileSystem {
+
+
+    private Disco disco;
+
+    private int[] fat = new int[16*1024];
+
+    public Fat32FS() throws IOException {
+        this.disco = new Disco();
+        if (this.disco.init()) {
+            leFat();
+            leDiretorio();
+        } else {
+            criaFat();
+            escreveFat();
+        }
+
+    }
+
+    private void criaFat() {
+        for (int i = 2; i < fat.length; i++) {
+            fat[i] = -1;
+        }
+    }
+
+    private void escreveFat() throws IOException {
+        ByteBuffer bb = ByteBuffer.allocate(64*1024);
+        for (int f : fat) {
+            bb.putInt(f);
+        }
+        byte[] blocoFat = bb.array();
+        disco.escreveBloco(BLOCO_FAT, blocoFat);
+    }
+
+    private static final int BLOCO_FAT = 1;
+    private void leFat() throws IOException {
+        byte[] blocoFat = disco.leBloco(BLOCO_FAT);
+        ByteBuffer bb = ByteBuffer.wrap(blocoFat);
+        for (int i = 0; i < 16*1024; i++) {
+            fat[i] = bb.getInt();
+        }
+    }
+
+    @Override
+    public void create(String fileName, byte[] data) {
+        if (data.length > freeSpace()) {
+            return;
+        }
+
+    }
+
+    private int encontraBlocoLivre() {
+        for (int i = 2; i < fat.length; i++) {
+            if (fat[i] == -1) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    @Override
+    public void append(String fileName, byte[] data) {
+
+    }
+
+    @Override
+    public byte[] read(String fileName, int offset, int limit) {
+        return new byte[0];
+    }
+
+
+    @Override
+    public void remove(String fileName) {
+
+    }
+
+    @Override
+    public int freeSpace() {
+        return 0;
+    }
+
     private static class EntradaArquivoDiretorio {
         private String nomeArquivo;
         private String extensao;
         private int tamanho;
         private int blocoInicial;
 
-         EntradaArquivoDiretorio(
-                String nomeArquivo,
-                String extensao,
-                int tamanho,
-                int blocoInicial
-        ) {
+        public EntradaArquivoDiretorio(String nomeArquivo,
+                                       String extensao,
+                                       int tamanho,
+                                       int blocoInicial) {
             this.nomeArquivo = nomeArquivo;
             if (this.nomeArquivo.length() > 8) {
                 this.nomeArquivo = nomeArquivo.substring(0, 8);
             } else if (this.nomeArquivo.length() < 8) {
                 do {
                     this.nomeArquivo += " ";
-                } while (this.nomeArquivo.length() < 3);
+                } while (this.nomeArquivo.length() < 8);
             }
-
             this.extensao = extensao;
             if (this.extensao.length() > 3) {
                 this.extensao = extensao.substring(0, 3);
@@ -40,25 +120,73 @@ public class Fat32FS {
             }
         }
 
-        public byte[] toByteArray() {
-            byte[] bytes = new byte[19];
-            System.arraycopy(nomeArquivo.getBytes(StandardCharsets.ISO_8859_1), 0, bytes,0, 8);
-            System.arraycopy(extensao.getBytes(StandardCharsets.ISO_8859_1), 0, bytes,8, 3);
-
-            System.arraycopy(intToBytes(tamanho), 0, bytes,11, 4);
-            System.arraycopy(intToBytes(blocoInicial), 0, bytes,15, 4);
-
-            return bytes;
-        }
-
-        public byte[] intToBytes(int i) {
-            ByteBuffer bb = ByteBuffer.allocate(4);
-            bb.putInt(i);
+        public byte[] toByteArray(ByteBuffer bb) {
+            bb.put(nomeArquivo.getBytes(StandardCharsets.ISO_8859_1));
+            bb.put(extensao.getBytes(StandardCharsets.ISO_8859_1));
+            bb.putInt(tamanho);
+            bb.putInt(blocoInicial);
             return bb.array();
         }
 
-        public static void main(String[] args) {
-            EntradaArquivoDiretorio entradaArquivoDiretorio = new EntradaArquivoDiretorio("arquivo", "txt")
+        private static int intFromBytes(byte[] data, int index) {
+            ByteBuffer bb = ByteBuffer.wrap(data);
+            return bb.getInt(index);
         }
+
+        public static EntradaArquivoDiretorio fromBytes(byte[] bytes) {
+            String nome = new String(bytes,
+                    0, 8, StandardCharsets.ISO_8859_1);
+            String extensao = new String(bytes,
+                    8, 3, StandardCharsets.ISO_8859_1);
+            int tamanho = intFromBytes(bytes, 11);
+            int blocoInicial = intFromBytes(bytes, 15);
+            System.out.println(nome);
+            System.out.println(extensao);
+            System.out.println(tamanho);
+            System.out.println(blocoInicial);
+            return new EntradaArquivoDiretorio(nome, extensao, tamanho, blocoInicial);
+        }
+
+        public static EntradaArquivoDiretorio fromStream(InputStream inputStream) throws IOException {
+            byte[] bytes = new byte[19];
+            inputStream.read(bytes);
+            String nome = new String(bytes,
+                    0, 8, StandardCharsets.ISO_8859_1);
+            String extensao = new String(bytes,
+                    8, 3, StandardCharsets.ISO_8859_1);
+            int tamanho = intFromBytes(bytes, 11);
+            int blocoInicial = intFromBytes(bytes, 15);
+            System.out.println(nome);
+            System.out.println(extensao);
+            System.out.println(tamanho);
+            System.out.println(blocoInicial);
+            return new EntradaArquivoDiretorio(nome, extensao, tamanho, blocoInicial);
+        }
+
     }
+
+    private static final int BLOCO_DIRETORIO = 0;
+    private List<EntradaArquivoDiretorio> diretorioRaiz = new ArrayList<>();
+
+    private void leDiretorio() throws IOException {
+        byte[] dirBytes = disco.leBloco(BLOCO_DIRETORIO);
+        ByteArrayInputStream bin = new ByteArrayInputStream(dirBytes);
+        EntradaArquivoDiretorio entrada = null;
+        do {
+            entrada = EntradaArquivoDiretorio.fromStream(bin);
+            if (entrada.tamanho > 0) {
+                diretorioRaiz.add(entrada);
+            }
+        } while(entrada.tamanho > 0);
+
+    }
+
+    private void escreveDiretorio() throws IOException {
+        ByteBuffer bb = ByteBuffer.allocate(Disco.TAM_BLOCO);
+        for (EntradaArquivoDiretorio entrada : diretorioRaiz) {
+            entrada.toByteArray(bb);
+        }
+        disco.escreveBloco(BLOCO_DIRETORIO, bb.array());
+    }
+
 }
